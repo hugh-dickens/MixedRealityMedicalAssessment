243e2ed9fda08c458d78deb48479d13e4282fc31
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-06-21 14:18:53 +0100

    Angular Updates

---
+++ ArUcoDetectionHoloLensUnity/Assets/Scripts/GetAngularVelocity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetAngularVelocity : MonoBehaviour
+{
+    public Vector3 Angular;
+    public TextMeshPro AngularPrinting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Angular = GetComponent<Rigidbody>().angularVelocity;
+        AngularPrinting.SetText(Angular.ToString());
+    }
+}
