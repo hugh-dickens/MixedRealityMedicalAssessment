4938ba85b636566d4ebc43079daef6265b64fab2
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-06-30 13:24:13 +0100

    EMG Sphere

---
+++ ArUcoDetectionHoloLensUnity/Assets/ChangeColour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color", new Color(0, 1, .2f)); // start green
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // find the UDP game object/ script so that EMG value can be found and used for colour of sphere
+        GameObject go = GameObject.Find("UDPComm");
+        UDPExperimenting cs = go.GetComponent<UDPExperimenting>();
+        float EMG_Colour = cs.EMG * 0.001f;
+        // new Color(redFloat, greenFloat, blueFloat)
+        // will customise this to reflect EMG high as red, low as green
+        // e.g. (redFloat * EMG, 1+(-greenFloat)*EMG, blueFloat)
+        rend.material.SetColor("_Color", new Color(0.2f * EMG_Colour, 1 + (-0.2f) * EMG_Colour, 0.2f));
+        
+    }
+}
---
+++ ArUcoDetectionHoloLensUnity/Assets/Scripts/UDPExperimenting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using TMPro;
+
+public class UDPExperimenting : MonoBehaviour
+{
+
+    public int EMG = 0;
+    public TextMeshPro EMG_Value;
+    public TextMeshPro Debugger_text;
+    public static void Main()
+    {
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9995);
+        UdpClient newsock = new UdpClient(ipep);
+
+        Console.WriteLine("Waiting for a client...");
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+        data = newsock.Receive(ref sender);
+
+        Console.WriteLine("Message received from {0}:", sender.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+        string welcome = "Welcome to my test server";
+        data = Encoding.ASCII.GetBytes(welcome);
+        newsock.Send(data, data.Length, sender);
+
+        while (true)
+        {
+            data = newsock.Receive(ref sender);
+
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+            newsock.Send(data, data.Length, sender);
+        }
+    }
+}
