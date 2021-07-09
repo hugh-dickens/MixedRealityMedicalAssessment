e9d18a83af6ceb1d3f6eafa398fa8d0251f19a24
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-07-09 17:48:57 +0100

    Update TCP_Polhemus.py

--- PythonScriptsForExperiment/TCP_Polhemus.py
+++ PythonScriptsForExperiment/TCP_Polhemus.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python
 import socket
 import struct
+import numpy as np
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 7234  # change to what polhemus is sending to
 BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-data = s.recv(BUFFER_SIZE)  # receives one packet and disconnects, modify into loop
-# will need to do some byte unpacking like other udp scripts
-s.close()
-# data = b'LY\x01C \x00"\x00\xe6\xe0\x03\x00\x13)\x10\x00\xc0<\xb6@L@(\xc0\xc9?~?\xc9j\xf6\xc2M!6@\x96\xb6\xd0A\r\n'
-# snum = struct.unpack('b', data[2:3])[0]
-# pno = [struct.unpack('f', data[i:i+4])[0] for i in range(8,29,4)]
+# s.close()
 
-output = []
-output.append(struct.unpack('b', data[2:3])[0])
-[output.append(struct.unpack('f', data[i:i+4])[0]) for i in range(16,37,4)]
-print(data)
-print(output)
+counter  = 0 
+while True:
+
+    output_sensor1 = []
+    output_sensor2 = []
+    output_sensor3 = []
+    counter +=1
+    data = s.recv(BUFFER_SIZE)
+    if counter % 500 ==0:
+        # output[0] = sensor number
+        # for x and y see the calibrater
+        # output[1] = x
+        # output[2] = y
+        # output[3] = z which is up and down
+        # output[4] = angle
+        # output[5] = angle
+        # output[6] = angle
+        output_sensor1.append(struct.unpack('b', data[2:3])[0])
+        [output_sensor1.append(struct.unpack('f', data[i:i+4])[0]) for i in range(16,37,4)]
+        output_sensor2.append(struct.unpack('b', data[44:45])[0])
+        [output_sensor2.append(struct.unpack('f', data[i:i+4])[0]) for i in range(58,79,4)]
+        output_sensor3.append(struct.unpack('b', data[86:87])[0])
+        [output_sensor3.append(struct.unpack('f', data[i:i+4])[0]) for i in range(100,121,4)]
+        
+        
+        a = np.array([output_sensor1[1], output_sensor1[2], output_sensor1[3]])
+        b = np.array([output_sensor2[1], output_sensor2[2], output_sensor2[3]])
+        c = np.array([output_sensor3[1], output_sensor3[2], output_sensor3[3]])
+
+        ba = a - b
+        bc = c - b
+
+        cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+        angle = np.arccos(cosine_angle)
+
+        print(np.degrees(angle))
+
+
+
