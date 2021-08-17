b7ccdea4a9b8ffde09ad8836b7fc2acbf1c9b766
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-08-17 13:13:57 +0100

    data

--- ExperimentalAnalysis/EditedScripts/MatlabScripts/CombinedAnglePlots.m
+++ ExperimentalAnalysis/EditedScripts/MatlabScripts/CombinedAnglePlots.m
@@ -6,7 +6,7 @@ clear;
 chk = exist('Nodes','var');
 if ~chk
      
-    ID = 4;
+    ID = 5;
     ID = num2str(ID);
     ID_folder = 'C:\MixedRealityDevelopment\CV4Holo\Hololens2ArUcoDetection\ExperimentalAnalysis\EditedScripts\Data_ID_';
     ID_folder =  [ID_folder ID '\'];
@@ -22,8 +22,8 @@ for i=1:20
        figure(i)
 % %     slow if statements
    
-        holo_dynamic = ['ID_4_medium_', num2str(i), '_HoloData'];
-        pol_dynamic = ['ID_4_medium_', num2str(i), '_POLGroundTruth'];
+        holo_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_HoloData'];
+        pol_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_POLGroundTruth'];
         
         if isfield(experiment_data,pol_dynamic) == 1
         Holo_data = experiment_data.(holo_dynamic);
@@ -43,7 +43,7 @@ for i=1:20
         xx_holo = x_holo(1):steps_holo:x_holo(length(x_holo));
         yy_holo = spline(x_holo,y_holo,xx_holo);
         
-%         subplot(2,1,1);
+        subplot(2,1,1);
         plot(x_holo,y_holo,'o',xx_holo,yy_holo);
         hold on
 
@@ -72,66 +72,306 @@ for i=1:20
         hold off
         % error bar part:
         
-%         Holo_data = experiment_data.(holo_dynamic);
-%         Pol_data = experiment_data.(pol_dynamic);
-% 
-%         holo_millisecond = round(Holo_data.Milliseconds,2,'significant');
-%         y_holo = Holo_data.Angle;
-%         
-%         holo_second = seconds(round(Holo_data.Timestamp, 'seconds'));
-%         Polh_second = seconds(round(Pol_data.Timestamp, 'seconds'));
-%         
-%         b1 = num2str(holo_second);
-%         b2 = num2str(holo_millisecond);
-%         % Concatenate the two strings element wise
-%         c1 = strcat(b1, b2);
-%         % turn spaces into 0s
-%         str = regexprep(cellstr(c1), ' ', '0');
-%         % Convert the result back to a numeric matrix
-%         x_holo = str2double(str);
-%         
-% 
-%         holo_data_final = cat(2,x_holo, y_holo);
-% 
-%         polh_millisecond = round(Pol_data.Milliseconds,2,'significant');
-%         polh_millisecond(polh_millisecond == 1000000) = 990000;
-%         
-%         
-%         a1 = num2str(Polh_second);
-%         a2 = num2str(polh_millisecond);
-%         % Concatenate the two strings element wise
-%         d1 = strcat(a1, a2);
-%         % turn spaces into 0s
-%         str1 = regexprep(cellstr(d1), ' ', '0');
-%         % Convert the result back to a numeric matrix
-%         x_pol = str2double(str1);
-% 
-%         % ADD THIS LINE FOR ALL TRIALS.
-%         x_pol = x_pol(1:length(sgf));
-% 
-%         pol_data_final = cat(2, x_pol, sgf);
-% 
-%         [~, rowsA, rowsB] = intersect(holo_data_final(:, 1), pol_data_final(:, 1));
-%         rowsA = sort(rowsA);
-%         rowsB = sort(rowsB);
-%         comparing_angles = [holo_data_final(rowsA, 2) pol_data_final(rowsB, 2)];
-% 
-%         comparing_diff = comparing_angles(:,1) - comparing_angles(:,2);        
-%         if length(comparing_diff)>1
-%             rmse = sqrt(mean((comparing_angles(:,1)-comparing_angles(:,2)).^2));
-%             subplot(2,1,2)
-%             bar(comparing_diff)
-%             title('Total rmse is',rmse)
-%             ylabel('Difference in angle data (holo - polh)')
-%         else 
-%             fprintf('No comparing diff data for trial %i; slow trial \n', i)
-%         end
+        Holo_data = experiment_data.(holo_dynamic);
+        Pol_data = experiment_data.(pol_dynamic);
+
+        holo_millisecond = round(Holo_data.Milliseconds,2,'significant');
+        y_holo = Holo_data.Angle;
+        
+        holo_second = seconds(round(Holo_data.Timestamp, 'seconds'));
+        Polh_second = seconds(round(Pol_data.Timestamp, 'seconds'));
+        
+        b1 = num2str(holo_second);
+        b2 = num2str(holo_millisecond);
+        % Concatenate the two strings element wise
+        c1 = strcat(b1, b2);
+        % turn spaces into 0s
+        str = regexprep(cellstr(c1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_holo = str2double(str);
+        
+
+        holo_data_final = cat(2,x_holo, y_holo);
+
+        polh_millisecond = round(Pol_data.Milliseconds,2,'significant');
+        polh_millisecond(polh_millisecond == 1000000) = 990000;
+        
+        
+        a1 = num2str(Polh_second);
+        a2 = num2str(polh_millisecond);
+        % Concatenate the two strings element wise
+        d1 = strcat(a1, a2);
+        % turn spaces into 0s
+        str1 = regexprep(cellstr(d1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_pol = str2double(str1);
+
+        % ADD THIS LINE FOR ALL TRIALS.
+        x_pol = x_pol(1:length(sgf));
+
+        pol_data_final = cat(2, x_pol, sgf);
+
+        [~, rowsA, rowsB] = intersect(holo_data_final(:, 1), pol_data_final(:, 1));
+        rowsA = sort(rowsA);
+        rowsB = sort(rowsB);
+        comparing_angles = [holo_data_final(rowsA, 2) pol_data_final(rowsB, 2)];
+
+        comparing_diff = comparing_angles(:,1) - comparing_angles(:,2);        
+        if length(comparing_diff)>1
+            rmse = sqrt(mean((comparing_angles(:,1)-comparing_angles(:,2)).^2));
+            subplot(2,1,2)
+            bar(comparing_diff)
+            title('Total rmse is',rmse)
+            ylabel('Difference in angle data (holo - polh)')
+        else 
+            fprintf('No comparing diff data for trial %i; slow trial \n', i)
+        end
         
         else
             fprintf('Not enough Hololens data for trial %i; slow trial \n',i)
         end
     else
         fprintf('No polhemus data for trial %i\n; slow trial \n',i)
+    end
+        
+end
+
+%% medium
+for i=1:20
+% i=1;
+       figure(i+20)
+% %     slow if statements
+   
+        holo_dynamic = ['ID_',num2str(ID),'_medium_', num2str(i), '_HoloData'];
+        pol_dynamic = ['ID_',num2str(ID),'_medium_', num2str(i), '_POLGroundTruth'];
+        
+        if isfield(experiment_data,pol_dynamic) == 1
+        Holo_data = experiment_data.(holo_dynamic);
+        Pol_data = experiment_data.(pol_dynamic);
+
+        % % plot holo data with points and a spline overlaid
+        x_holo = seconds(Holo_data.Timestamp);
+        y_holo = Holo_data.Angle;
+        if length(y_holo) > 1
+        more_rowsToDelete =  x_holo > (x_holo(1)+1000);
+        rowsToDelete = y_holo < 0 | y_holo > 180;
+        y_holo(rowsToDelete) = [];
+        x_holo(rowsToDelete) = [];
+        y_holo(more_rowsToDelete) = [];
+        x_holo(more_rowsToDelete) = [];
+        steps_holo = (x_holo(length(x_holo)) - x_holo(1)) / sum(x_holo);
+        xx_holo = x_holo(1):steps_holo:x_holo(length(x_holo));
+        yy_holo = spline(x_holo,y_holo,xx_holo);
+        
+        subplot(2,1,1);
+        plot(x_holo,y_holo,'o',xx_holo,yy_holo);
+        hold on
+
+        % % plot holo data with points and a spline overlaid
+        x_pol = seconds(Pol_data.Timestamp);
+        y_pol = Pol_data.Angle;
+        rowsToDelete = y_pol < 0 | y_pol > 180;
+        more_rowsToDelete = x_pol > (x_pol(1)+1000);
+        y_pol(more_rowsToDelete) = [];
+        x_pol(more_rowsToDelete) = [];
+        y_pol(rowsToDelete) = [];
+        x_pol(rowsToDelete) = [];
+        % filter the polh data before plotting....
+        order = 3;
+        framelen = 93;
+
+        sgf = sgolayfilt(y_pol,order,framelen);
+        
+        plot(x_pol, sgf);
+% 
+        xlabel('Time')
+        ylabel('Angle')
+        title('Medium trial')
+        legend('Holo Data','Holo Spline','Polh Data')
+        
+        hold off
+        % error bar part:
+        
+        Holo_data = experiment_data.(holo_dynamic);
+        Pol_data = experiment_data.(pol_dynamic);
+
+        holo_millisecond = round(Holo_data.Milliseconds,2,'significant');
+        y_holo = Holo_data.Angle;
+        
+        holo_second = seconds(round(Holo_data.Timestamp, 'seconds'));
+        Polh_second = seconds(round(Pol_data.Timestamp, 'seconds'));
+        
+        b1 = num2str(holo_second);
+        b2 = num2str(holo_millisecond);
+        % Concatenate the two strings element wise
+        c1 = strcat(b1, b2);
+        % turn spaces into 0s
+        str = regexprep(cellstr(c1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_holo = str2double(str);
+        
+
+        holo_data_final = cat(2,x_holo, y_holo);
+
+        polh_millisecond = round(Pol_data.Milliseconds,2,'significant');
+        polh_millisecond(polh_millisecond == 1000000) = 990000;
+        
+        
+        a1 = num2str(Polh_second);
+        a2 = num2str(polh_millisecond);
+        % Concatenate the two strings element wise
+        d1 = strcat(a1, a2);
+        % turn spaces into 0s
+        str1 = regexprep(cellstr(d1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_pol = str2double(str1);
+
+        % ADD THIS LINE FOR ALL TRIALS.
+        x_pol = x_pol(1:length(sgf));
+
+        pol_data_final = cat(2, x_pol, sgf);
+
+        [~, rowsA, rowsB] = intersect(holo_data_final(:, 1), pol_data_final(:, 1));
+        rowsA = sort(rowsA);
+        rowsB = sort(rowsB);
+        comparing_angles = [holo_data_final(rowsA, 2) pol_data_final(rowsB, 2)];
+
+        comparing_diff = comparing_angles(:,1) - comparing_angles(:,2);        
+        if length(comparing_diff)>1
+            rmse = sqrt(mean((comparing_angles(:,1)-comparing_angles(:,2)).^2));
+            subplot(2,1,2)
+            bar(comparing_diff)
+            title('Total rmse is',rmse)
+            ylabel('Difference in angle data (holo - polh)')
+        else 
+            fprintf('No comparing diff data for trial %i; medium trial \n', i)
+        end
+        
+        else
+            fprintf('Not enough Hololens data for trial %i; medium trial \n',i)
+        end
+    else
+        fprintf('No polhemus data for trial %i\n; medium trial \n',i)
+    end
+        
+end
+
+%% fast
+for i=1:20
+% i=1;
+       figure(i+40)
+% %     slow if statements
+   
+        holo_dynamic = ['ID_',num2str(ID),'_fast_', num2str(i), '_HoloData'];
+        pol_dynamic = ['ID_',num2str(ID),'_fast_', num2str(i), '_POLGroundTruth'];
+        
+        if isfield(experiment_data,pol_dynamic) == 1
+        Holo_data = experiment_data.(holo_dynamic);
+        Pol_data = experiment_data.(pol_dynamic);
+
+        % % plot holo data with points and a spline overlaid
+        x_holo = seconds(Holo_data.Timestamp);
+        y_holo = Holo_data.Angle;
+        if length(y_holo) > 1
+        more_rowsToDelete =  x_holo > (x_holo(1)+1000);
+        rowsToDelete = y_holo < 0 | y_holo > 180;
+        y_holo(rowsToDelete) = [];
+        x_holo(rowsToDelete) = [];
+        y_holo(more_rowsToDelete) = [];
+        x_holo(more_rowsToDelete) = [];
+        steps_holo = (x_holo(length(x_holo)) - x_holo(1)) / sum(x_holo);
+        xx_holo = x_holo(1):steps_holo:x_holo(length(x_holo));
+        yy_holo = spline(x_holo,y_holo,xx_holo);
+        
+        subplot(2,1,1);
+        plot(x_holo,y_holo,'o',xx_holo,yy_holo);
+        hold on
+
+        % % plot holo data with points and a spline overlaid
+        x_pol = seconds(Pol_data.Timestamp);
+        y_pol = Pol_data.Angle;
+        rowsToDelete = y_pol < 0 | y_pol > 180;
+        more_rowsToDelete = x_pol > (x_pol(1)+1000);
+        y_pol(more_rowsToDelete) = [];
+        x_pol(more_rowsToDelete) = [];
+        y_pol(rowsToDelete) = [];
+        x_pol(rowsToDelete) = [];
+        % filter the polh data before plotting....
+        order = 3;
+        framelen = 93;
+
+        sgf = sgolayfilt(y_pol,order,framelen);
+        
+        plot(x_pol, sgf);
+% 
+        xlabel('Time')
+        ylabel('Angle')
+        title('Fast trial')
+        legend('Holo Data','Holo Spline','Polh Data')
+        
+        hold off
+        % error bar part:
+        
+        Holo_data = experiment_data.(holo_dynamic);
+        Pol_data = experiment_data.(pol_dynamic);
+
+        holo_millisecond = round(Holo_data.Milliseconds,2,'significant');
+        y_holo = Holo_data.Angle;
+        
+        holo_second = seconds(round(Holo_data.Timestamp, 'seconds'));
+        Polh_second = seconds(round(Pol_data.Timestamp, 'seconds'));
+        
+        b1 = num2str(holo_second);
+        b2 = num2str(holo_millisecond);
+        % Concatenate the two strings element wise
+        c1 = strcat(b1, b2);
+        % turn spaces into 0s
+        str = regexprep(cellstr(c1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_holo = str2double(str);
+        
+
+        holo_data_final = cat(2,x_holo, y_holo);
+
+        polh_millisecond = round(Pol_data.Milliseconds,2,'significant');
+        polh_millisecond(polh_millisecond == 1000000) = 990000;
+        
+        
+        a1 = num2str(Polh_second);
+        a2 = num2str(polh_millisecond);
+        % Concatenate the two strings element wise
+        d1 = strcat(a1, a2);
+        % turn spaces into 0s
+        str1 = regexprep(cellstr(d1), ' ', '0');
+        % Convert the result back to a numeric matrix
+        x_pol = str2double(str1);
+
+        % ADD THIS LINE FOR ALL TRIALS.
+        x_pol = x_pol(1:length(sgf));
+
+        pol_data_final = cat(2, x_pol, sgf);
+
+        [~, rowsA, rowsB] = intersect(holo_data_final(:, 1), pol_data_final(:, 1));
+        rowsA = sort(rowsA);
+        rowsB = sort(rowsB);
+        comparing_angles = [holo_data_final(rowsA, 2) pol_data_final(rowsB, 2)];
+
+        comparing_diff = comparing_angles(:,1) - comparing_angles(:,2);        
+        if length(comparing_diff)>1
+            rmse = sqrt(mean((comparing_angles(:,1)-comparing_angles(:,2)).^2));
+            subplot(2,1,2)
+            bar(comparing_diff)
+            title('Total rmse is',rmse)
+            ylabel('Difference in angle data (holo - polh)')
+        else 
+            fprintf('No comparing diff data for trial %i; fast trial \n', i)
+        end
+        
+        else
+            fprintf('Not enough Hololens data for trial %i; fast trial \n',i)
+        end
+    else
+        fprintf('No polhemus data for trial %i\n; fast trial \n',i)
     end
         
 end
