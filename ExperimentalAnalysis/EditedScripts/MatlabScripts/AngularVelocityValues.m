07e69b30b11b138059966ce265bea2212992140e
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-08-19 13:54:29 +0100

    updates

--- ExperimentalAnalysis/EditedScripts/MatlabScripts/AngularVelocityValues.m
+++ ExperimentalAnalysis/EditedScripts/MatlabScripts/AngularVelocityValues.m
@@ -26,37 +26,42 @@ medium_filteredStruct = rmfield( experiment_data, names( find( cellfun( @isempty
 subStrFast = '_fast';
 fast_filteredStruct = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast ) ) ) ) );
 
-%% second recordings
-
-pol_missing_data_v2 = [];
-% names = fieldnames( experiment_data );
-subStrSlow_v2 = '_slowv2';
-slow_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v2 ) ) ) ) );
-subStrMedium_v2 = '_mediumv2';
-medium_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v2 ) ) ) ) );
-subStrFast_v2 = '_fastv2';
-fast_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v2 ) ) ) ) );
-
-
-%% third recordings
-
-pol_missing_data_v3 = [];
-% names = fieldnames( experiment_data );
-subStrSlow_v3 = '_slowv3';
-slow_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v3 ) ) ) ) );
-subStrMedium_v3 = '_mediumv3';
-medium_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v3 ) ) ) ) );
-subStrFast_v3 = '_fastv3';
-fast_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v3 ) ) ) ) );
-
-
+% %% second recordings
+% 
+% pol_missing_data_v2 = [];
+% % names = fieldnames( experiment_data );
+% subStrSlow_v2 = '_slowv2';
+% slow_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v2 ) ) ) ) );
+% subStrMedium_v2 = '_mediumv2';
+% medium_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v2 ) ) ) ) );
+% subStrFast_v2 = '_fastv2';
+% fast_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v2 ) ) ) ) );
+% 
+% 
+% %% third recordings
+% 
+% pol_missing_data_v3 = [];
+% % names = fieldnames( experiment_data );
+% subStrSlow_v3 = '_slowv3';
+% slow_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v3 ) ) ) ) );
+% subStrMedium_v3 = '_mediumv3';
+% medium_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v3 ) ) ) ) );
+% subStrFast_v3 = '_fastv3';
+% fast_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v3 ) ) ) ) );
+% 
+% 
 %% slow
 
 namesSlow = fieldnames( slow_filteredStruct );
 subStrPol = '_POLGroundTruth';
 Pol_filteredStruct = rmfield( slow_filteredStruct, namesSlow(find(cellfun(@isempty, strfind( namesSlow, subStrPol)))));
 Polh_Fields = fieldnames(Pol_filteredStruct);
+subStrHolo = '_HoloData';
+Holo_filteredStruct = rmfield( slow_filteredStruct, namesSlow(find(cellfun(@isempty, strfind( namesSlow, subStrHolo)))));
+Holo_Fields = fieldnames(Holo_filteredStruct);
 
+
+baseStr = erase(pol_dynamic,subStrPol)
 
 vels_cell_slow = cell(length(Polh_Fields), 2);
 for trialnum = 1:length(Polh_Fields)
@@ -110,126 +115,126 @@ for trialnum = 1:length(Polh_Fields)
         end
         
 end
-
-%% medium
-namesMedium = fieldnames( medium_filteredStruct );
-subStrPol = '_POLGroundTruth';
-Pol_filteredStruct = rmfield( medium_filteredStruct, namesMedium(find(cellfun(@isempty, strfind( namesMedium, subStrPol)))));
-Polh_Fields = fieldnames(Pol_filteredStruct);
-
-vels_cell_medium = cell(length(Polh_Fields), 2);
-for trialnum = 1:length(Polh_Fields)
-    
-    pol_dynamic = [string(Polh_Fields(trialnum))] ;
-
-        if isfield(experiment_data,pol_dynamic) == 1
-            Pol_data = experiment_data.(pol_dynamic);
-        
-        try
-        x_pol = seconds(Pol_data.Timestamp);
-        y_pol = Pol_data.Angle;
-        pol_millis = Pol_data.Milliseconds;
-        
-        rowsToDelete = y_pol < 0 | y_pol > 180;
-        more_rowsToDelete = x_pol > (x_pol(1)+1000);
-        y_pol(more_rowsToDelete) = [];
-        x_pol(more_rowsToDelete) = [];
-        y_pol(rowsToDelete) = [];
-        x_pol(rowsToDelete) = []; 
-        pol_millis(more_rowsToDelete) = [];
-        pol_millis(rowsToDelete) = [];
-        
-        order = 3;
-        framelen = 93;
-
-        sgf = sgolayfilt(y_pol,order,framelen);
-        
-        v = zeros(length(pol_millis),1) ;
-        for i = 1:length(pol_millis)-1
-            v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
-        end
-        
-        max_inst_vel = find(v==max(v));
-        start_ind = max_inst_vel - 150;
-        end_ind = max_inst_vel + 150;
-        if end_ind < length(v) & start_ind > 150
-            avg_vel = mean(v(start_ind:end_ind));
-        elseif end_ind > length(v) & start_ind > 150
-            avg_vel = mean(v(start_ind:end));
-        else
-            avg_vel = 0;
-            fprintf('No avg vel data trial %i\n', trialnum)
-        end
-        
-        vels_cell_medium{trialnum, 1}  = pol_dynamic;
-        vels_cell_medium{trialnum,2} = avg_vel;
-       catch me
-            fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
-        end
-    else
-        fprintf('No polhemus data for trial %i\n; medium trial \n',trialnum)
-        end
-        
-end
-
-%% fast
-namesFast = fieldnames( fast_filteredStruct );
-subStrPol = '_POLGroundTruth';
-Pol_filteredStruct = rmfield( fast_filteredStruct, namesFast(find(cellfun(@isempty, strfind( namesFast, subStrPol)))));
-Polh_Fields = fieldnames(Pol_filteredStruct);
-
-vels_cell_fast = cell(length(Polh_Fields), 2);
-for trialnum = 1:length(Polh_Fields)
-    
-    pol_dynamic = [string(Polh_Fields(trialnum))] ;
-
-        if isfield(experiment_data,pol_dynamic) == 1
-            Pol_data = experiment_data.(pol_dynamic);
-        try           
-        x_pol = seconds(Pol_data.Timestamp);
-        y_pol = Pol_data.Angle;
-        pol_millis = Pol_data.Milliseconds;
-        
-        rowsToDelete = y_pol < 0 | y_pol > 180;
-        more_rowsToDelete = x_pol > (x_pol(1)+1000);
-        y_pol(more_rowsToDelete) = [];
-        x_pol(more_rowsToDelete) = [];
-        y_pol(rowsToDelete) = [];
-        x_pol(rowsToDelete) = []; 
-        pol_millis(more_rowsToDelete) = [];
-        pol_millis(rowsToDelete) = [];
-        
-        order = 3;
-        framelen = 93;
-
-        sgf = sgolayfilt(y_pol,order,framelen);
-        
-        v = zeros(length(pol_millis),1) ;
-        for i = 1:length(pol_millis)-1
-            v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
-        end
-        
-        max_inst_vel = find(v==max(v));
-        start_ind = max_inst_vel - 150;
-        end_ind = max_inst_vel + 150;
-        if end_ind < length(v) & start_ind > 150
-            avg_vel = mean(v(start_ind:end_ind));
-        elseif end_ind > length(v) & start_ind > 150
-            avg_vel = mean(v(start_ind:end));
-        else
-            avg_vel = 0;
-            fprintf('No avg vel data trial %i\n', trialnum)
-        end
-        
-        vels_cell_fast{trialnum, 1}  = pol_dynamic;
-        vels_cell_fast{trialnum,2} = avg_vel;
-        
-        catch me
-            fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
-        end
-       
-    else
-        fprintf('No polhemus data for trial %i\n; fast trial \n',i)
-        end
-        
-end
+% 
+% %% medium
+% namesMedium = fieldnames( medium_filteredStruct );
+% subStrPol = '_POLGroundTruth';
+% Pol_filteredStruct = rmfield( medium_filteredStruct, namesMedium(find(cellfun(@isempty, strfind( namesMedium, subStrPol)))));
+% Polh_Fields = fieldnames(Pol_filteredStruct);
+% 
+% vels_cell_medium = cell(length(Polh_Fields), 2);
+% for trialnum = 1:length(Polh_Fields)
+%     
+%     pol_dynamic = [string(Polh_Fields(trialnum))] ;
+% 
+%         if isfield(experiment_data,pol_dynamic) == 1
+%             Pol_data = experiment_data.(pol_dynamic);
+%         
+%         try
+%         x_pol = seconds(Pol_data.Timestamp);
+%         y_pol = Pol_data.Angle;
+%         pol_millis = Pol_data.Milliseconds;
+%         
+%         rowsToDelete = y_pol < 0 | y_pol > 180;
+%         more_rowsToDelete = x_pol > (x_pol(1)+1000);
+%         y_pol(more_rowsToDelete) = [];
+%         x_pol(more_rowsToDelete) = [];
+%         y_pol(rowsToDelete) = [];
+%         x_pol(rowsToDelete) = []; 
+%         pol_millis(more_rowsToDelete) = [];
+%         pol_millis(rowsToDelete) = [];
+%         
+%         order = 3;
+%         framelen = 93;
+% 
+%         sgf = sgolayfilt(y_pol,order,framelen);
+%         
+%         v = zeros(length(pol_millis),1) ;
+%         for i = 1:length(pol_millis)-1
+%             v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
+%         end
+%         
+%         max_inst_vel = find(v==max(v));
+%         start_ind = max_inst_vel - 150;
+%         end_ind = max_inst_vel + 150;
+%         if end_ind < length(v) & start_ind > 150
+%             avg_vel = mean(v(start_ind:end_ind));
+%         elseif end_ind > length(v) & start_ind > 150
+%             avg_vel = mean(v(start_ind:end));
+%         else
+%             avg_vel = 0;
+%             fprintf('No avg vel data trial %i\n', trialnum)
+%         end
+%         
+%         vels_cell_medium{trialnum, 1}  = pol_dynamic;
+%         vels_cell_medium{trialnum,2} = avg_vel;
+%        catch me
+%             fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
+%         end
+%     else
+%         fprintf('No polhemus data for trial %i\n; medium trial \n',trialnum)
+%         end
+%         
+% end
+% 
+% %% fast
+% namesFast = fieldnames( fast_filteredStruct );
+% subStrPol = '_POLGroundTruth';
+% Pol_filteredStruct = rmfield( fast_filteredStruct, namesFast(find(cellfun(@isempty, strfind( namesFast, subStrPol)))));
+% Polh_Fields = fieldnames(Pol_filteredStruct);
+% 
+% vels_cell_fast = cell(length(Polh_Fields), 2);
+% for trialnum = 1:length(Polh_Fields)
+%     
+%     pol_dynamic = [string(Polh_Fields(trialnum))] ;
+% 
+%         if isfield(experiment_data,pol_dynamic) == 1
+%             Pol_data = experiment_data.(pol_dynamic);
+%         try           
+%         x_pol = seconds(Pol_data.Timestamp);
+%         y_pol = Pol_data.Angle;
+%         pol_millis = Pol_data.Milliseconds;
+%         
+%         rowsToDelete = y_pol < 0 | y_pol > 180;
+%         more_rowsToDelete = x_pol > (x_pol(1)+1000);
+%         y_pol(more_rowsToDelete) = [];
+%         x_pol(more_rowsToDelete) = [];
+%         y_pol(rowsToDelete) = [];
+%         x_pol(rowsToDelete) = []; 
+%         pol_millis(more_rowsToDelete) = [];
+%         pol_millis(rowsToDelete) = [];
+%         
+%         order = 3;
+%         framelen = 93;
+% 
+%         sgf = sgolayfilt(y_pol,order,framelen);
+%         
+%         v = zeros(length(pol_millis),1) ;
+%         for i = 1:length(pol_millis)-1
+%             v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
+%         end
+%         
+%         max_inst_vel = find(v==max(v));
+%         start_ind = max_inst_vel - 150;
+%         end_ind = max_inst_vel + 150;
+%         if end_ind < length(v) & start_ind > 150
+%             avg_vel = mean(v(start_ind:end_ind));
+%         elseif end_ind > length(v) & start_ind > 150
+%             avg_vel = mean(v(start_ind:end));
+%         else
+%             avg_vel = 0;
+%             fprintf('No avg vel data trial %i\n', trialnum)
+%         end
+%         
+%         vels_cell_fast{trialnum, 1}  = pol_dynamic;
+%         vels_cell_fast{trialnum,2} = avg_vel;
+%         
+%         catch me
+%             fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
+%         end
+%        
+%     else
+%         fprintf('No polhemus data for trial %i\n; fast trial \n',i)
+%         end
+%         
+% end
