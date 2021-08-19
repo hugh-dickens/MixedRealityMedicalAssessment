461a2ec9bfafeb0cb5ce8465f60e3a1451ad085a
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-08-19 13:12:31 +0100

    matlab updates

--- ExperimentalAnalysis/EditedScripts/MatlabScripts/AngularVelocityValues.m
+++ ExperimentalAnalysis/EditedScripts/MatlabScripts/AngularVelocityValues.m
@@ -5,7 +5,7 @@ clear all;
 chk = exist('Nodes','var');
 if ~chk
      
-    ID = 10;
+    ID = 9;
     ID = num2str(ID);
     ID_folder = 'C:\MixedRealityDevelopment\CV4Holo\Hololens2ArUcoDetection\ExperimentalAnalysis\EditedScripts\Data_MATLAB';
     ID_folder =  [ID_folder '\'];
@@ -29,13 +29,25 @@ fast_filteredStruct = rmfield( experiment_data, names( find( cellfun( @isempty, 
 %% second recordings
 
 pol_missing_data_v2 = [];
-names = fieldnames( experiment_data );
+% names = fieldnames( experiment_data );
 subStrSlow_v2 = '_slowv2';
 slow_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v2 ) ) ) ) );
 subStrMedium_v2 = '_mediumv2';
-medium_filteredStruct = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v2 ) ) ) ) );
+medium_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v2 ) ) ) ) );
 subStrFast_v2 = '_fastv2';
 fast_filteredStruct_v2 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v2 ) ) ) ) );
+
+
+%% third recordings
+
+pol_missing_data_v3 = [];
+% names = fieldnames( experiment_data );
+subStrSlow_v3 = '_slowv3';
+slow_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrSlow_v3 ) ) ) ) );
+subStrMedium_v3 = '_mediumv3';
+medium_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrMedium_v3 ) ) ) ) );
+subStrFast_v3 = '_fastv3';
+fast_filteredStruct_v3 = rmfield( experiment_data, names( find( cellfun( @isempty, strfind( names , subStrFast_v3 ) ) ) ) );
 
 
 %% slow
@@ -50,32 +62,11 @@ vels_cell_slow = cell(length(Polh_Fields), 2);
 for trialnum = 1:length(Polh_Fields)
     
     pol_dynamic = [string(Polh_Fields(trialnum))] ;
-% for i=1:30
-% i=1;
-%        figure(trialnum)
-% %     slow if statements
-%    
-%         holo_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_HoloData'];
-%         pol_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_POLGroundTruth'];
-%         
+
         if isfield(experiment_data,pol_dynamic) == 1
             Pol_data = experiment_data.(pol_dynamic);
             
-            % % plot holo data with points and a spline overlaid
-        x_pol = seconds(Pol_data.Timestamp);
-        y_pol = Pol_data.Angle;
-        rowsToDelete = y_pol < 0 | y_pol > 180;
-        more_rowsToDelete = x_pol > (x_pol(1)+1000);
-        y_pol(more_rowsToDelete) = [];
-        x_pol(more_rowsToDelete) = [];
-        y_pol(rowsToDelete) = [];
-        x_pol(rowsToDelete) = [];
-        % filter the polh data before plotting....
-        order = 3;
-        framelen = 93;
 
-        sgf = sgolayfilt(y_pol,order,framelen);
-        
         x_pol = seconds(Pol_data.Timestamp);
         y_pol = Pol_data.Angle;
         pol_millis = Pol_data.Milliseconds;
@@ -99,7 +90,18 @@ for trialnum = 1:length(Polh_Fields)
             v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
         end
         
-        avg_vel = mean(v);
+        max_inst_vel = find(v==max(v));
+        start_ind = max_inst_vel - 150;
+        end_ind = max_inst_vel + 150;
+        if end_ind < length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end_ind));
+        elseif end_ind > length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end));
+        else
+            avg_vel = 0;
+            fprintf('No avg vel data trial %i\n', trialnum)
+        end
+            
         vels_cell_slow{trialnum, 1}  = pol_dynamic;
         vels_cell_slow{trialnum,2} = avg_vel;
        
@@ -119,32 +121,11 @@ vels_cell_medium = cell(length(Polh_Fields), 2);
 for trialnum = 1:length(Polh_Fields)
     
     pol_dynamic = [string(Polh_Fields(trialnum))] ;
-% for i=1:30
-% i=1;
-%        figure(trialnum)
-% %     slow if statements
-%    
-%         holo_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_HoloData'];
-%         pol_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_POLGroundTruth'];
-%         
+
         if isfield(experiment_data,pol_dynamic) == 1
             Pol_data = experiment_data.(pol_dynamic);
-            
-            % % plot holo data with points and a spline overlaid
-        x_pol = seconds(Pol_data.Timestamp);
-        y_pol = Pol_data.Angle;
-        rowsToDelete = y_pol < 0 | y_pol > 180;
-        more_rowsToDelete = x_pol > (x_pol(1)+1000);
-        y_pol(more_rowsToDelete) = [];
-        x_pol(more_rowsToDelete) = [];
-        y_pol(rowsToDelete) = [];
-        x_pol(rowsToDelete) = [];
-        % filter the polh data before plotting....
-        order = 3;
-        framelen = 93;
-
-        sgf = sgolayfilt(y_pol,order,framelen);
         
+        try
         x_pol = seconds(Pol_data.Timestamp);
         y_pol = Pol_data.Angle;
         pol_millis = Pol_data.Milliseconds;
@@ -168,12 +149,25 @@ for trialnum = 1:length(Polh_Fields)
             v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
         end
         
-        avg_vel = mean(v);
+        max_inst_vel = find(v==max(v));
+        start_ind = max_inst_vel - 150;
+        end_ind = max_inst_vel + 150;
+        if end_ind < length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end_ind));
+        elseif end_ind > length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end));
+        else
+            avg_vel = 0;
+            fprintf('No avg vel data trial %i\n', trialnum)
+        end
+        
         vels_cell_medium{trialnum, 1}  = pol_dynamic;
         vels_cell_medium{trialnum,2} = avg_vel;
-       
+       catch me
+            fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
+        end
     else
-        fprintf('No polhemus data for trial %i\n; medium trial \n',i)
+        fprintf('No polhemus data for trial %i\n; medium trial \n',trialnum)
         end
         
 end
@@ -188,32 +182,10 @@ vels_cell_fast = cell(length(Polh_Fields), 2);
 for trialnum = 1:length(Polh_Fields)
     
     pol_dynamic = [string(Polh_Fields(trialnum))] ;
-% for i=1:30
-% i=1;
-%        figure(trialnum)
-% %     slow if statements
-%    
-%         holo_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_HoloData'];
-%         pol_dynamic = ['ID_',num2str(ID),'_slow_', num2str(i), '_POLGroundTruth'];
-%         
+
         if isfield(experiment_data,pol_dynamic) == 1
             Pol_data = experiment_data.(pol_dynamic);
-            
-            % % plot holo data with points and a spline overlaid
-        x_pol = seconds(Pol_data.Timestamp);
-        y_pol = Pol_data.Angle;
-        rowsToDelete = y_pol < 0 | y_pol > 180;
-        more_rowsToDelete = x_pol > (x_pol(1)+1000);
-        y_pol(more_rowsToDelete) = [];
-        x_pol(more_rowsToDelete) = [];
-        y_pol(rowsToDelete) = [];
-        x_pol(rowsToDelete) = [];
-        % filter the polh data before plotting....
-        order = 3;
-        framelen = 93;
-
-        sgf = sgolayfilt(y_pol,order,framelen);
-        
+        try           
         x_pol = seconds(Pol_data.Timestamp);
         y_pol = Pol_data.Angle;
         pol_millis = Pol_data.Milliseconds;
@@ -237,9 +209,24 @@ for trialnum = 1:length(Polh_Fields)
             v(i) = abs((sgf(i+1)-sgf(i))/(pol_millis(i+1)-pol_millis(i)) * 1000000);
         end
         
-        avg_vel = mean(v);
+        max_inst_vel = find(v==max(v));
+        start_ind = max_inst_vel - 150;
+        end_ind = max_inst_vel + 150;
+        if end_ind < length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end_ind));
+        elseif end_ind > length(v) & start_ind > 150
+            avg_vel = mean(v(start_ind:end));
+        else
+            avg_vel = 0;
+            fprintf('No avg vel data trial %i\n', trialnum)
+        end
+        
         vels_cell_fast{trialnum, 1}  = pol_dynamic;
         vels_cell_fast{trialnum,2} = avg_vel;
+        
+        catch me
+            fprintf('Line 128: Input data must be a real, numeric array. %i\n', trialnum);
+        end
        
     else
         fprintf('No polhemus data for trial %i\n; fast trial \n',i)
